cdd520363b1f8dcd5e78cfc66b793a22a67a6af7
hwi1018 <hwi101810@gmail.com>
2023-11-03 22:13:11 +0900

    bookmark service

---
+++ bookmark/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+
+class Bookmark(models.Model):
+    def __str__(self):
+        return "이름: " + self.site_name + ", 주소: " + self.url
+
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
+
+    site_name = models.CharField(max_length=200)
+    url = models.URLField('Site URL')
